158fc1d9eeedb7271dca49af9d0aecfcfd9eee9a
Younggil Lee <leyoungg@sheridancollege.ca>
2017-05-16 13:29:27 -0400

    set remote

---
+++ src/finalprojectgame/Recent.java
@@ -0,0 +1,19 @@
+package finalProjectGame;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Recent.
+ */
+public class Recent extends Game {
+
+	/**
+	 * Instantiates a new recent.
+	 *
+	 * @param type
+	 *            the type
+	 */
+	public Recent(String type) {
+		super(type);
+	}
+
+}
